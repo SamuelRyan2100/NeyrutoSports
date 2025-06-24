67cebe813bf04b34bfc3a25b11e1f474926962ad
Samuel Ryan <samuelryan2100@gmail.com>
2025-06-24 09:40:01 -0300

    Refatorando o site

--- src/frontend/public/js/main.js
+++
@@ -1,7 +0,0 @@
-async function carregarLogin() {
-  const response = await fetch('../../components/login.html');
-  const html = await response.text();
-  document.getElementById('app').innerHTML = html;
-}
-
-carregarLogin(); // Carrega a tela de login ao iniciar
