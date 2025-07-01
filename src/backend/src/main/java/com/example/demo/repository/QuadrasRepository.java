35b0f74b05283de52121accc02c3d52f03d41123
Samuel Ryan <samuelryan2100@gmail.com>
2025-07-01 09:48:50 -0300

    adiconando ligação docker

--- src/backend/src/main/java/com/example/demo/repository/QuadrasRepository.java
+++ src/backend/src/main/java/com/example/demo/repository/QuadrasRepository.java
@@ -3,14 +3,21 @@ package com.example.demo.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.model.Quadras;
+
+import jakarta.transaction.Transactional;
 
 @Repository
 public interface QuadrasRepository extends JpaRepository<Quadras, Integer> {
 	Quadras findByNome(String nome);
 
-	
+    @Modifying
+    @Transactional
+    @Query("UPDATE Quadras q SET q.ativa = false WHERE q.ativa = true")
+    int desativarTodasAtivas();
 	//List<Quadras> findAll();
 }
